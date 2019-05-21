4dc3b13b3550eddabdc91bbd467d892f615cb71e
Fadgiras <mayeuxgudinaflorian@gmail.com>
2019-05-21 17:18:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <HTML>
   <?php
   session_start();
+  $_SESSION['Niv'] = 0//test Niv
   ?>
   <head>
     <meta charset="utf-8">
@@ -17,32 +18,32 @@
       <ul>
       <li><a href="index.php"> Accueil </a></li>
       <?php
-      if ($_SESSION['niv'] > 0){
+      if ($_SESSION['Niv'] > 0){
         echo '<li>Panel Admin<ul>';
         //include_once("ConnexionBDD.php");
-        //$prof_req = $dbh->query('SELECT niv FROM Utilisateur WHERE id = $_SESSION['id']');
-        $prof_req = 2
-          if ($prof_req === 2){
-            ?><li><a href="GCompte.php">Gestion Des Comptes</a></li><?php
-          }
+        //$prof_req = $dbh->query('SELECT Niv FROM Utilisateur WHERE id = $_SESSION['id']');
+        $prof_req = 2;//test admin
+        if ($prof_req === 2){
+          ?><li><a href="GCompte.php">Gestion Des Comptes</a></li>
+          <?php
         }
-        ?><li><a href="GFichier.php">Gestion Des Fichiers</a></li>
+        ?>
+        <li><a href="GFichier.php">Gestion Des Fichiers</a></li>
         <li><a href="GCours.php">Mise en ligne des Cours</a></li>
-        </li><?php
+      <?php
       }
-      
       ?>
       <li><a> Cours </a>
         <?php
         include_once("ConnexionBDD.php");
-        $prof_req = $dbh->query('SELECT nom FROM Utilisateur WHERE niv = 1');
+        $prof_req = $dbh->query('SELECT nom FROM Utilisateur WHERE Niv = 1 OR Id=1');
         $prof_list = $prof_req->fetchAll();
         foreach ($prof_list as $prof){
-        echo '<option value="'.$prof[0].'">'.$prof[1].'</option>';
+        echo '<select value="'.$prof[0].'">'.$prof[1].'</select>';
           }
         ?>
       </li>
-      <li><a href="compte.php"> Connexion </a></li>
+      <li><a href="compte.php"> Compte </a></li>
       <li><a href="contact.php"> Contacter&nbsp;un&nbsp;professeur</a></li>
       </ul>
     </nav>
