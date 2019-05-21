425e6485494d10de794608154881959fb62d1666
Fadgiras <mayeuxgudinaflorian@gmail.com>
2019-05-21 17:52:39 +0200

    Created Compte.php and Header.php, Rearranged Index.php

---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<HTML>
+<body>
+  <header>
+    <a href="./index.php"><img src="images/banner.png" /></a>
+    <nav class="menubar">
+      <ul>
+      <li><a href="index.php"> Accueil </a></li>
+      <?php
+      if ($_SESSION['Niv'] > 0){
+        echo '<li>Panel Admin<ul>';
+        //include_once("ConnexionBDD.php");
+        //$prof_req = $dbh->query('SELECT Niv FROM Utilisateur WHERE id = $_SESSION['id']');
+        $prof_req = 2;//test admin
+        if ($prof_req === 2){
+          ?><li><a href="GCompte.php">Gestion Des Comptes</a></li>
+          <?php
+        }
+        ?>
+        <li><a href="GFichier.php">Gestion Des Fichiers</a></li>
+        <li><a href="GCours.php">Mise en ligne des Cours</a></li>
+      <?php
+      }
+      ?>
+      <li><a> Cours </a>
+        <?php
+        include_once("ConnexionBDD.php");
+        $prof_req = $dbh->query('SELECT nom FROM Utilisateur WHERE Niv = 1 OR Id=1');
+        $prof_list = $prof_req->fetchAll();
+        foreach ($prof_list as $prof){
+        echo '<select value="'.$prof[0].'">'.$prof[1].'</select>';
+          }
+        ?>
+      </li>
+      <li><a href="compte.php"> Compte </a></li>
+      <li><a href="contact.php"> Contacter&nbsp;un&nbsp;professeur</a></li>
+      </ul>
+    </nav>
+  </header>
+</body>
+<HTML>
