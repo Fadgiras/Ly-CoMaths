fe7b5bff169e6fff7a551898ba4ae0c448df0d73
Fadgiras <mayeuxgudinaflorian@gmail.com>
2019-05-21 11:32:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,50 @@
+<!DOCTYPE HTML>
 <HTML>
-<?php
-session_start();
-?>
-<head>
-  <meta charset="utf-8">
-  <title> Ly-CoMaths </title>
-  <!--<link rel="stylesheet" href="style.css">-->
-  <link rel="stylesheet" href="style/common.css">
-</head>
-<body>
-<header>
-  <a href="./index.php"><img src="images/banner.png" /></a>
-  
-  <nav class="menubar">
-    <ul>
-    <li><a href="index.php"> Accueil </a></li>
-	  <?php
-    if ($_SESSION['niv'] === 1 || $_SESSION['niv'] === 2){
-      echo '<li><a>Panel Admin</a>';
-      include_once("ConnexionBDD.php");
-      $prof_req = $dbh->query('SELECT nom FROM Utilisateur WHERE niv = 1');
-      $prof_list = $prof_req->fetchAll();
-      foreach ($prof_list as $prof){
-        echo '<option value="'.$prof[0].'">'.$prof[1].'</option>';
-      }
-      echo '</li>'
-    }
-    
-    ?>
-    <li><a> Cours </a>
+  <?php
+  session_start();
+  ?>
+  <head>
+    <meta charset="utf-8">
+    <title> Ly-CoMaths </title>
+    <!--<link rel="stylesheet" href="style.css">-->
+    <link rel="stylesheet" href="style/common.css">
+  </head>
+  <body>
+  <header>
+    <a href="./index.php"><img src="images/banner.png" /></a>
+
+    <nav class="menubar">
+      <ul>
+      <li><a href="index.php"> Accueil </a></li>
       <?php
-      include_once("ConnexionBDD.php");
-      $prof_req = $dbh->query('SELECT nom FROM Utilisateur WHERE niv = 1');
-      $prof_list = $prof_req->fetchAll();
-      foreach ($prof_list as $prof){
-      echo '<option value="'.$prof[0].'">'.$prof[1].'</option>';
+      if ($_SESSION['niv'] > 0){
+        echo '<li>Panel Admin<ul>';
+        //include_once("ConnexionBDD.php");
+        //$prof_req = $dbh->query('SELECT niv FROM Utilisateur WHERE id = $_SESSION['id']');
+        $prof_req = 2
+          if ($prof_req === 2){
+            ?><li><a href="GCompte.php">Gestion Des Comptes</a></li><?php
+          }
         }
+        ?><li><a href="GFichier.php">Gestion Des Fichiers</a></li>
+        <li><a href="GCours.php">Mise en ligne des Cours</a></li>
+        </li><?php
+      }
+      
       ?>
-    </li>
-    <li><a href="compte.php"> Connexion </a></li>
-    <li><a href="contact.php"> Contacter&nbsp;un&nbsp;professeur</a></li>
-    </ul>
-  </nav>
-</header>
+      <li><a> Cours </a>
+        <?php
+        include_once("ConnexionBDD.php");
+        $prof_req = $dbh->query('SELECT nom FROM Utilisateur WHERE niv = 1');
+        $prof_list = $prof_req->fetchAll();
+        foreach ($prof_list as $prof){
+        echo '<option value="'.$prof[0].'">'.$prof[1].'</option>';
+          }
+        ?>
+      </li>
+      <li><a href="compte.php"> Connexion </a></li>
+      <li><a href="contact.php"> Contacter&nbsp;un&nbsp;professeur</a></li>
+      </ul>
+    </nav>
+  </header>
 </HTML>
